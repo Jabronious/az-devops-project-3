b5cdc5d54d7f15df3b11d2a34bb4793771d608f9
Jarrod Merryman <jarrodmerryman@gmail.com>
2021-08-20 19:40:35 -0500

    debug mode

--- terraform/environment/test/main.tf
+++ terraform/environment/test/main.tf
@@ -6,6 +6,13 @@ provider "azurerm" {
   features {}
 }
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.50.0"
+    }
+  }
+
   backend "azurerm" {
     storage_account_name = "tfstate383"
     container_name       = "tfstate"
