dab5b0feeb545fcc76c2b001a0cb1768b1f657a3
Jarrod Merryman <jarrodmerryman@gmail.com>
2021-08-21 20:22:24 -0500

    nupdate vm

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,7 +20,7 @@ resource "azurerm_linux_virtual_machine" "az-vm" {
   network_interface_ids = [azurerm_network_interface.az-nic.id]
   admin_ssh_key {
     username   = "${var.admin_username}"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("~/.ssh/terraform_rsa.pub")
   }
   os_disk {
     caching           = "ReadWrite"
