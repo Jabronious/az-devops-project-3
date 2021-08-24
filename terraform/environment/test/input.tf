ef5d43748c4f3407b9a1b2a6554b375e96494c64
Jarrod Merryman <jarrodmerryman@gmail.com>
2021-08-24 16:42:09 -0500

    resolves issue with app name

--- terraform/environment/test/input.tf
+++ terraform/environment/test/input.tf
@@ -8,7 +8,6 @@ variable "tenant_id" {}
 variable "location" {}
 variable "resource_group" {}
 variable "application_type" {}
-variable "application_name" {}
 
 # Network
 variable virtual_network_name {}
