bcc9d0c1eee45e9ebd1c79ec1638c6865db54047
Jarrod Merryman <jarrodmerryman@gmail.com>
2021-08-27 20:21:46 -0500

    adds the selenium tests required for piepline

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -2,17 +2,42 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
-
+DRIVER = webdriver.Chrome()
 # Start the browser and login with standard_user
 def login (user, password):
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
     # options = ChromeOptions()
     # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    # DRIVER = webdriver.Chrome(options=options)
     print ('Browser started successfully. Navigating to the demo page to login.')
-    driver.get('https://www.saucedemo.com/')
+    DRIVER.get('https://www.saucedemo.com/')
+
+    DRIVER.find_element_by_id('user-name').send_keys(user)
+    DRIVER.find_element_by_id('password').send_keys(password)
+    DRIVER.find_element_by_id('login-button').click()
+
+    print ('Asserting PRODUCTS is the page title')
+    # .upper ensures the case will be matched in the event that the website decides to change the title formatting
+    assert 'PRODUCTS'.upper() in DRIVER.find_element_by_class_name('title').text.upper()
+    print ('Assertion passed.')
+
+def add_cart():
+    print('adding back pack...')
+    DRIVER.find_element_by_id('add-to-cart-sauce-labs-backpack').click()
+    print ('Asserting back pack was added to cart')
+    assert '1' in DRIVER.find_element_by_class_name('shopping_cart_badge').text
+    print ('Assertion passed.')
+
+def remove_cart():
+    print('removing back pack...')
+    DRIVER.find_element_by_id('remove-sauce-labs-backpack').click()
+    print ('Asserting back pack was removed from cart')
+    assert not len(DRIVER.find_elements_by_class_name('shopping_cart_badge'))
+    print ('Assertion passed.')
 
 login('standard_user', 'secret_sauce')
+add_cart()
+remove_cart()
 
+DRIVER.quit()
