feba5caa9ea2d3fe8d6182525f283d7a4d35e5f7
Jarrod Merryman <jarrodmerryman@gmail.com>
2021-09-04 21:19:40 -0500

    changes logs slightly

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -9,37 +9,37 @@ chrome_options.add_argument("--headless")
 DRIVER = webdriver.Chrome(options=chrome_options)
 # Start the browser and login with standard_user
 def login (user, password):
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' - Starting the browser...')
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\tStarting the browser...')
     DRIVER.get('https://www.saucedemo.com/')
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' - Browser started successfully. Navigating to the demo page to login.')
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\tBrowser started successfully. Navigating to the demo page to login.')
 
     DRIVER.find_element_by_id('user-name').send_keys(user)
     DRIVER.find_element_by_id('password').send_keys(password)
     DRIVER.find_element_by_id('login-button').click()
 
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' - %s logged in' % user)
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' - Asserting PRODUCTS is the page title')
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\t%s logged in' % user)
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\tAsserting PRODUCTS is the page title')
     # .upper ensures the case will be matched in the event that the website decides to change the title formatting
     assert 'PRODUCTS'.upper() in DRIVER.find_element_by_class_name('title').text.upper()
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' - Assertion passed.')
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\tAssertion passed.')
 
 def add_cart():
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' - Asserting items were added to cart')
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\tAsserting items were added to cart')
     assert str(click_btns()) in DRIVER.find_element_by_class_name('shopping_cart_badge').text
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' - Assertion passed.')
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\tAssertion passed.')
 
 def remove_cart():
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' - Asserting back pack was removed from cart')
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\tAsserting back pack was removed from cart')
     click_btns()
     assert not len(DRIVER.find_elements_by_class_name('shopping_cart_badge'))
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' - Assertion passed.')
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\tAssertion passed.')
 
 def click_btns():
     inventoryBtns = DRIVER.find_elements_by_class_name('btn_inventory')
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' - Clicking all the buttons...')
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\tClicking all the buttons...')
     for btn in inventoryBtns:
         btn.click()
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' - %d items clicked' % len(inventoryBtns))
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\t%d items clicked' % len(inventoryBtns))
     return len(inventoryBtns)
 
 login('standard_user', 'secret_sauce')
